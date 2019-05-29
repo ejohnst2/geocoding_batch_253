b5a8c224a2197849b49934141fe11bf7b47bde43
elijah johnston <eli@millinno.com>
2019-05-29 10:48:16 +0800

    autocomplete init with mapbox geocoder and custom styles on map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,11 @@
 import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+
 // import the function from file
 import { initMapbox } from '../plugins/init_mapbox';
+import { initAutocomplete } from '../plugins/init_autocomplete';
 
 // call the function
 initMapbox();
-
+initAutocomplete();
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,4 +1,6 @@
 import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+
 
 // this function sets the boundaries of the map
 // fits those boundaries to the map element on the page
@@ -16,16 +18,21 @@ const initMapbox = () => {
     // creating new instance of MapBox map
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
+      style: 'mapbox://styles/ejohnst2/cjw8ml1mj09rn1bs65vybctqg'
     });
     // create new variable for markers, from the data in HTML
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
+     // create a new instance of MB popup based on info window
+     const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
       new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup)
         .addTo(map);
     });
     fitMapToMarkers(map, markers);
+    // adding the geocoder search to the map
+    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken }));
   }
 };
 
